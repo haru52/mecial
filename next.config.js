da4853f9f7ef46c59ecb25de4d1f73a25ec76350
haru <haru@haru52.com>
2024-05-15 22:37:42 +0900

    chore: add Google auth

--- next.config.js
+++ next.config.js
@@ -10,9 +10,15 @@ const config = {
     remotePatterns: [
       {
         protocol: "https",
-        hostname: "cdn.discordapp.com",
+        hostname: "lh3.googleusercontent.com",
         port: "",
         pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "cdn.discordapp.com",
+        port: "",
+        pathname: "/avatars/**",
       },
     ],
   },
