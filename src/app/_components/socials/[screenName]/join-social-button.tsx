9adad91f89130542031bae79e1d9825e4f33c1cf
haru <haru@haru52.com>
2024-05-17 01:22:37 +0900

    chore: fix format

--- src/app/_components/socials/[screenName]/join-social-button.tsx
+++ src/app/_components/socials/[screenName]/join-social-button.tsx
@@ -4,11 +4,7 @@ import type { Social } from "~/entities/social";
 import { api } from "~/trpc/react";
 import { useRouter } from "next/navigation";
 
-export function JoinSocialButton({
-  social,
-}: {
-  social: Social;
-}) {
+export function JoinSocialButton({ social }: { social: Social }) {
   const router = useRouter();
   const { mutate } = api.avatar.create.useMutation({
     onSuccess: () => {
