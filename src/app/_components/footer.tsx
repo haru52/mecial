e4dc5d042e7e5cc72f9b9cf168dd610368d1bd3f
haru <haru@haru52.com>
2024-05-18 19:58:12 +0900

    chore: update footer

--- src/app/_components/footer.tsx
+++ src/app/_components/footer.tsx
@@ -6,8 +6,12 @@ export function Footer() {
   return (
     <footer className="footer footer-center rounded bg-base-200 p-10 text-base-content">
       <nav className="grid grid-flow-col gap-4">
-        <Link href="/terms-of-service">利用規約</Link>
-        <Link href="/privacy-policy">プライバシーポリシー</Link>
+        <Link href="/terms-of-service" className="hover:underline">
+          利用規約
+        </Link>
+        <Link href="/privacy-policy" className="hover:underline">
+          プライバシーポリシー
+        </Link>
       </nav>
       <nav>
         <div className="grid grid-flow-col gap-4">
@@ -23,7 +27,12 @@ export function Footer() {
       <aside>
         <p>
           Copyright © 2024 - All right reserved by{" "}
-          <Link href="https://haru52.com/" target="_blank" rel="noreferrer">
+          <Link
+            href="https://haru52.com/"
+            target="_blank"
+            rel="noreferrer"
+            className="hover:underline"
+          >
             haru
           </Link>
         </p>
