19cb90ce6bc9e8f0081d64b9b38dfa34e0d1a3d2
haru <haru@haru52.com>
2024-05-24 03:21:37 +0900

    chore: update

--- src/app/privacy-policy/page.tsx
+++ src/app/privacy-policy/page.tsx
@@ -2,7 +2,7 @@ export default function Page() {
   return (
     <main className="container prose mx-auto px-4">
       <h1>プライバシーポリシー</h1>
-      <p className="text-right">2024 年 5 月 18 日</p>
+      <p className="text-right">2024 年 5 月 24 日</p>
       <ol>
         <li>
           当サイトはユーザーがログインに使用した Google または Discord
