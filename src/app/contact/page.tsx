6c43e58d2f9990109b38ba2ad8d2b9cfaa72ea99
haru <haru@haru52.com>
2024-06-21 00:39:24 +0900

    chore: update contact

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -4,7 +4,8 @@ export default function Contact() {
       <h1 className="text-center">お問い合わせ</h1>
       <iframe
         src="https://docs.google.com/forms/d/e/1FAIpQLScSwihmWcb3gJzK9_bQ9mpNLwhdpoQdILcNH1dqhj7-MHQtcA/viewform?embedded=true"
-        className="h-screen w-full"
+        className="w-full"
+        height={1276}
         title="Mecial お問い合わせフォーム"
         loading="lazy"
       ></iframe>
