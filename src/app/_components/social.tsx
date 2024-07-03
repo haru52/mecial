aa97ce039a31ac1ebce04db5231f8b58ec147a3e
haru <haru@haru52.com>
2024-07-04 00:57:22 +0900

    chore: update files

--- src/app/_components/social.tsx
+++ src/app/_components/social.tsx
@@ -18,22 +18,27 @@ export function Social({
   return (
     <Link
       href={`/socials/${social.screenName}`}
-      className="mx-auto block w-full no-underline"
+      className="w-full no-underline"
     >
       <div className="card w-full bg-base-100 shadow-xl">
         <div className="card-body">
-          <div className="not-prose avatar">
-            <div className="w-11 rounded-full">
-              <Image
-                src={social.image ?? defaultSocialIconPath}
-                width={500}
-                height={500}
-                alt=""
-              />
+          <div className="card-title">
+            <div className="not-prose avatar">
+              <div className="w-11 rounded-full">
+                <Image
+                  src={social.image ?? defaultSocialIconPath}
+                  width={500}
+                  height={500}
+                  alt=""
+                />
+              </div>
+            </div>
+            <div>
+              <h2 className="card-title my-0">{social.name}</h2>
+              <p className="my-0 text-sm">{social.screenName}</p>
             </div>
           </div>
-          <h2 className="card-title">{social.name}</h2>
-          <p>{social.description}</p>
+          <p className="my-0">{social.description}</p>
           {isLoggedIn && (
             <div className="card-actions justify-end">
               {avatar === undefined ? (
