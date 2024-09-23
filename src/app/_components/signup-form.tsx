f05f91e0636184ab5c4a36f2624d0524578f01d4
haru <haru@haru52.com>
2024-09-23 23:35:41 +0900

    chore: update signup

--- src/app/_components/signup-form.tsx
+++ src/app/_components/signup-form.tsx
@@ -6,10 +6,19 @@ import { useState } from "react";
 import { InputErrorMessages } from "~/app/_components/input-error-messages";
 import { api } from "~/trpc/react";
 import { screenNameRule } from "~/consts";
+import type { User } from "~/entities/user";
 
-export function SignupForm() {
+function validate(screenName: string, name: string | null) {
+  return (
+    screenName.trim().length > 0 && name !== null && name.trim().length > 0
+  );
+}
+
+export function SignupForm({ user }: { user: User }) {
   const router = useRouter();
   const [screenName, setScreenName] = useState("");
+  const [name, setName] = useState(user.name);
+  const [image, setImage] = useState(user.image);
   const [isDisabled, setIsDisabled] = useState(false);
   const { mutate, error } = api.user.update.useMutation({
     onSuccess: () => {
@@ -27,6 +36,15 @@ export function SignupForm() {
   if (error !== null && error?.data?.zodError == null)
     screenNameErrors.push(error.message);
 
+  const nameErrors: string[] = [];
+  if (error?.data?.zodError?.fieldErrors.name !== undefined) {
+    nameErrors.push(...error.data.zodError.fieldErrors.name);
+  }
+  const imageErrors: string[] = [];
+  if (error?.data?.zodError?.fieldErrors.image !== undefined) {
+    imageErrors.push(...error.data.zodError.fieldErrors.image);
+  }
+
   return (
     <form
       onSubmit={async (e) => {
@@ -34,6 +52,9 @@ export function SignupForm() {
         setIsDisabled(true);
         mutate({
           screenName,
+          name,
+          image:
+            image === null ? null : image.trim().length === 0 ? null : image,
         });
       }}
     >
@@ -41,8 +62,7 @@ export function SignupForm() {
         <span className="label label-text">ID</span>
         <input
           type="text"
-          placeholder="taroyamada"
-          className={`input input-bordered w-full max-w-xs ${clsx({ "input-error": screenNameErrors !== undefined })}`}
+          className={`input input-bordered w-full max-w-xs ${clsx({ "input-error": screenNameErrors !== undefined && screenNameErrors.length > 0 })}`}
           value={screenName ?? ""}
           onChange={(e) => setScreenName(e.target.value)}
           required
@@ -50,12 +70,33 @@ export function SignupForm() {
         <span className="label label-text-alt">{screenNameRule}</span>
         <InputErrorMessages errMsgs={screenNameErrors} />
       </label>
+      <label className="form-control mx-auto w-full max-w-xs">
+        <span className="label label-text">名前</span>
+        <input
+          type="text"
+          className={`input input-bordered w-full max-w-xs ${clsx({ "input-error": nameErrors !== undefined && nameErrors.length > 0 })}`}
+          value={name ?? ""}
+          onChange={(e) => setName(e.target.value)}
+          required
+        />
+        <InputErrorMessages errMsgs={nameErrors} />
+      </label>
+      <label className="form-control mx-auto w-full max-w-xs">
+        <span className="label label-text">アイコン画像URL</span>
+        <input
+          type="text"
+          className={`input input-bordered w-full max-w-xs ${clsx({ "input-error": imageErrors !== undefined && imageErrors.length > 0 })}`}
+          value={image ?? ""}
+          onChange={(e) => setImage(e.target.value)}
+        />
+        <InputErrorMessages errMsgs={imageErrors} />
+      </label>
       <div className="form-control mx-auto mt-7 w-full max-w-xs">
         <input
           type="submit"
           value={isDisabled ? "サインアップ中…" : "サインアップ"}
           className="btn btn-primary btn-block rounded-full"
-          disabled={isDisabled || screenName.trim().length === 0}
+          disabled={isDisabled || !validate(screenName, name)}
         />
       </div>
     </form>
--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -1,15 +1,20 @@
 import { SignupForm } from "../_components/signup-form";
 import { getServerAuthSession } from "~/server/auth";
+import { api } from "~/trpc/server";
 
 export default async function Page() {
   const session = await getServerAuthSession();
   if (session === null) {
     return null;
   }
+  const user = await api.user.getMe();
+  if (user === null) {
+    return null;
+  }
   return (
     <div className="container prose mx-auto mb-10 mt-5 px-4">
-      <h1>サインアップ</h1>
-      <SignupForm />
+      <h1 className="text-center">サインアップ</h1>
+      <SignupForm user={user} />
     </div>
   );
 }
